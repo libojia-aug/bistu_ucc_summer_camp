c8bbeb4faf847b8ea6aea0ef1171e387e81b2676
libojia-aug <1012139091@qq.com>
2018-07-24 14:50:12 +0100

    modified:   Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,5 +1,19 @@
-a=0
-b=1
-while b < 1000:
-    print(b)
-    a, b = b, a+b
+def maxNum(maxNum=1000):
+    a = 0
+    b = 1
+    while b < maxNum:
+        print(b)
+        a, b = b, a + b
+
+
+def maxCount(maxCount=100):
+    a = 0
+    b = 1
+    count = 0
+    while count < maxCount:
+        print(b)
+        a, b = b, a + b
+        count = count + 1
+
+maxNum(100)
+maxCount(10)
